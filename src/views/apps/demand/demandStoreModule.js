a34ad2ac2166d1abe6fe20e90ccc4d5ed985dc33
Saliou SARR <m2sproduction@hotmail.fr>
2021-03-04 08:12:43 +0100

    Add dynamic update for demand list

--- src/views/apps/demand/demandStoreModule.js
+++ src/views/apps/demand/demandStoreModule.js
@@ -10,9 +10,9 @@ export default {
   getters: {},
   mutations: {
     SET_DEMANDS: (state, list) => state.demands = list,
-    SET_DEMAND_FIELD(state, field, { demandId, value }) {
-      const index = state.findIndex(demand => demand.id === demandId)
-      if (index) Vue.set(state.demands[index], field, value)
+    SET_DEMAND_FIELD(state, { demandId, field, value }) {
+      const index = state.demands.findIndex(demand => demand.id === demandId)
+      if (index !== undefined) Vue.set(state.demands[index], field, value)
       else console.warn(`Index not found while adding ${field} in demands list`)
     }
 
@@ -28,18 +28,17 @@ export default {
     async assignCollaborator({ commit }, {demandId, userId}) {
       const res = await $http.post(`/demands/assignTo/${demandId}`, { userId })
       if (res && res.data) {
-        commit('SET_DEMAND_FIELD','handler', { demandId, value: {userId} })
+        commit('SET_DEMAND_FIELD', { demandId, field: 'handler', value: res.data })
         return res.data
       }
     },
     async addComment({ commit }, { demandId, comment }) {
       const res = await $http.post(`demands/addComment/${demandId}`, { comment })
       if (res && res.data) {
-        commit('SET_DEMAND_FIELD','comment', { demandId, value: comment })
+        commit('SET_DEMAND_FIELD', { demandId, field: 'comment', value: res.data })
         return res.data
       }
-      //TODO: Uncomment when the back will provide the new comment object
-      // throw new Error('Comment failed to register')
+      throw new Error('Comment failed to register')
     },
     fetchUser(ctx, { id }) {
       return new Promise((resolve, reject) => {
